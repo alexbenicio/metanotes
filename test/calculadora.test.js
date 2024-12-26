5ebfd605fa83d77e920d8b1d781acf6a53a3ef9c
alexbenicio <165957234+alexbenicio@users.noreply.github.com>
2024-12-26 14:13:51 +0000

    Add teste apenas para exercicio

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,3 +1,13 @@
+const calculadora = require('../models/calculadora');
+
 test('Teste simples', () => {
-  expect(3).toBe(3);
+  expect(calculadora.somar(1, 2)).toBe(3);
+});
+
+test('Teste simples', () => {
+  expect(calculadora.somar(2, 2)).toBe(4);
+});
+
+test('Teste simples', () => {
+  expect(calculadora.somar(2, 8)).toBe(10);
 });
